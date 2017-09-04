2deba05d152b6302355db4635e6fd1da9b738332
Sabrina <sabrinacarlew@gmail.com>
2017-09-04 16:47:39 -0300

    Auto stash before merge of "master" and "origin/master"
    H1, erro na linha de instancia do objeto tarefa

--- TrabalhoPP2/src/heuristicas/H1.java
+++ TrabalhoPP2/src/heuristicas/H1.java
@@ -5,10 +5,60 @@
  */
 package heuristicas;
 
+import gerais.LeitorArquivo;
+import gerais.Tarefa;
+import java.util.ArrayList;
+
 /**
  *
- * @author Lucas
+ * @author Sabrina
  */
 public class H1 {
+    private Tarefa ep [][];
+    private int vertice;
+
+    public H1(int tamanho) {
+        this.ep = new Tarefa [tamanho][tamanho];
+    }
     
+    public void preencheH1(){//Não está pronto.
+        ArrayList<Tarefa> listaTarefas = LeitorArquivo.montarLista(LeitorArquivo.carregarArquivo());          
+        Tarefa tarefaM = listaTarefas.get(0);
+        Tarefa tarefa = new Tarefa(0);
+        boolean mestra = false;
+
+        for(int i = 0; i< ep.length; i++){
+            for (int j = 0; j<ep.length; i++){
+                if(tarefa.getNumero() == tarefaM.getMestra()){
+                    mestra = true;
+                }
+                if(ep [i][j]== null && mestra == true){
+                    ep [i][j] = tarefaM;
+                    mestra = false;      
+                }
+                while(ep[i][j] == tarefaM){
+                    if(ep [i][j+1] == null){
+                         ep [i][j+1] = tarefa;
+                         break;
+                    }
+                    if(j > 0){
+                        if(ep [i][j-1] == null){
+                            ep [i][j-1] = tarefa;
+                            break;
+                        }
+                    }
+                    if(i>0){
+                        if(ep [i-1][j] == null){
+                            ep [i-1][j] = tarefa;
+                            break;
+                        }
+                    }
+                    if(ep [i+1][j] == null){
+                          ep [i+1][j] = tarefa;
+                          break;
+                    }
+                }
+            }
+        }
+    }
 }
