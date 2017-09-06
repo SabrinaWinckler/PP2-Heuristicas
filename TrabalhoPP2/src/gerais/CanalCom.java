7ed989c5010ad3be272de29adb7246b9fa8149e0
Lucas Martins <lgatts@gmail.com>
2017-09-06 16:03:43 -0300

    Moficação Classe Aplicativo

---
+++ TrabalhoPP2/src/gerais/CanalCom.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gerais;
+
+import java.util.List;
+
+/**
+ *
+ * @author Lucas
+ */
+public class CanalCom {
+    Tarefa tarefa;
+    List<CanalCom> listaCanais;
+    int i;
+    int j;
+    
+    public CanalCom(int i, int j){
+        
+    }
+
+    public Tarefa getTarefa() {
+        return tarefa;
+    }
+
+    public List<CanalCom> getListaCanais() {
+        return listaCanais;
+    }
+
+    public int getI() {
+        return i;
+    }
+
+    public int getJ() {
+        return j;
+    }
+    
+    
+    
+}
--- TrabalhoPP2/src/view/View.java
+++ TrabalhoPP2/src/view/View.java
@@ -5,10 +5,84 @@
  */
 package view;
 
+import java.util.ArrayList;
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+
 /**
  *
  * @author Lucas
  */
-public class View {
-    
+public class View extends Application {
+
+    Group group;
+    Stage stage;
+    Scene scene;
+    Button button;
+    ArrayList<Rectangle> list;
+
+    int contador;
+
+    @Override
+    public void start(Stage stage) {
+
+        list = new ArrayList();
+
+        this.stage = stage;
+        stage.setTitle("MPSoc");
+        group = new Group();
+        scene = new Scene(group, 800, 600);
+        stage.setScene(scene);
+
+        button = new Button("Start");
+        button.setLayoutX(740);
+        button.setLayoutY(570);
+
+        group.getChildren().add(button);
+
+        contador = 0;
+
+        stage.show();
+
+        button.setOnAction(event -> {
+            buttonClick();
+        });
+
+    }
+
+    public void printTela() {
+
+        Label label = new Label(String.valueOf(contador));
+        label.setLayoutX(contador * 100);
+        
+        list.add(new Rectangle(contador * 100, 0, 30, 30));
+        list.get(contador).setFill(Color.BLUE);
+
+        group.getChildren().add(label);
+        group.getChildren().add(list.get(contador));
+        
+        label.toFront();
+
+        stage.show();
+    }
+
+    public void buttonClick() {
+
+        printTela();
+        contador++;
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
 }
