4a4b74de95967f072c23e8d725efa9ea06953980
Rodrigo Oliveira <roliveira.loki@gmail.com>
2017-09-06 16:17:42 -0300

    feito matrix

--- TrabalhoPP2/src/heuristicas/FF.java
+++ TrabalhoPP2/src/heuristicas/FF.java
@@ -5,10 +5,36 @@
  */
 package heuristicas;
 
+import gerais.LeitorArquivo;
+import gerais.Aplicativo;
+import gerais.Tarefa;
+
 /**
  *
  * @author Lucas
  */
 public class FF {
+    private Tarefa[][] mat;
+
+    public FF(Tarefa[][] mat) {
+        this.mat = mat;
+    }
+    
+    public FF(int tamanho) {
+        mat = new Tarefa[tamanho][tamanho];
+    }        
+    
+    
+    
+    public void add(Tarefa task) {
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat.length; j++){
+                if (mat[i][j] == null) {
+                mat[i][j] = task;
+                }
+            }
+        }
+    }
+    
     
 }
