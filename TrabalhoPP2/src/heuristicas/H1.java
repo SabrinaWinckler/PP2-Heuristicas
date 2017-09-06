73de55801f84eae526b1f1bb0b1d9cdb57682446
Sabrina <sabrinacarlew@gmail.com>
2017-09-06 16:50:40 -0300

    H1 modificação

--- TrabalhoPP2/src/heuristicas/H1.java
+++ TrabalhoPP2/src/heuristicas/H1.java
@@ -17,7 +17,9 @@ import java.util.List;
  */
 public class H1 {
     private Tarefa ep [][];
-    private int vertice;
+    private int verticeIda;
+    private int verticeVolta;
+    private int identificador;
 
     public H1(int tamanho) {
         this.ep = new Tarefa [tamanho][tamanho];
@@ -27,42 +29,39 @@ public class H1 {
         ArrayList<Aplicativo> listaAplicativos = LeitorArquivo.montarLista(LeitorArquivo.carregarArquivo());          
         List<Tarefa> listaTarefas = listaAplicativos.get(0).getTarefas();
         Tarefa tarefa = new Tarefa(0);
-        Tarefa tmestra;
-        boolean mestra = false;
 
         for(int i = 0; i< ep.length; i++){
-            for (int j = 0; j<ep.length; i++){
-                if(tarefa.getNumero() == listaTarefas.get(i).getMestra()){
-                    mestra = true;
-                    tmestra = listaTarefas.get(i);
-                }
-                if(mestra == true && ep [i][j]== null){
+            this.identificador = listaTarefas.get(i).getNumero();
+            this.verticeIda = listaTarefas.get(i).getLarguraIda();
+            this.verticeVolta = listaTarefas.get(i).getLarguraVolta();
+            int tarefaMestra = listaTarefas.get(i).getMestra();
+                for (int j = 0; j<ep.length; i++){
+                if(this.identificador == tarefaMestra && ep [i][j]== null){
                     ep [i][j] = tarefa;
-                    mestra = false;      
+                }                                             
+                if(ep [i][j+1] == null){
+                    ep [i][j+1] = tarefa;
+                    break;
                 }
-                while(ep[i][j] ==listaTarefas.get(i)){
-                    if(ep [i][j+1] == null){
-                         ep [i][j+1] = tarefa;
-                         break;
+                if(j > 0){
+                    if(ep [i][j-1] == null){
+                    ep [i][j-1] = tarefa;
+                    break;
                     }
-                    if(j > 0){
-                        if(ep [i][j-1] == null){
-                            ep [i][j-1] = tarefa;
-                            break;
-                        }
+                }
+                if(i>0){
+                    if(ep [i-1][j] == null){
+                        ep [i-1][j] = tarefa;
+                        break;
                     }
-                    if(i>0){
-                        if(ep [i-1][j] == null){
-                            ep [i-1][j] = tarefa;
-                            break;
-                        }
-                    }
-                    if(ep [i+1][j] == null){
-                          ep [i+1][j] = tarefa;
-                          break;
-                    }
+                }
+                if(ep [i+1][j] == null){
+                
+                   ep [i+1][j] = tarefa;
+                    break;
                 }
             }
-        }
+       }
     }
 }
+
